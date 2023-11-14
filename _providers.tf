6f08a8a4d48bd3be624b7baa42ca73a1ea2fa0ac
Caio Fernandes <caiovf13@gmail.com>
2023-11-14 18:23:21 -0300

    First commit

---
+++ _providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {}
+
+# Use Databricks CLI authentication.
+provider "databricks" {}
